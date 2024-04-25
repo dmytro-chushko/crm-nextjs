ffe9c3ade34a5155e22aa1a9e0899b2f5903b764
dmytro-chushko <dmytro.chushko@gmail.com>
2024-04-25 19:17:40 +0300

    Add dynamic route

---
+++ src/app/components/toolbar.tsx
@@ -0,0 +1,15 @@
+interface IToolbarProps {
+  children: React.ReactNode;
+  action?: React.ReactNode;
+}
+
+const Toolbar = ({ children, action }: IToolbarProps) => {
+  return (
+    <div className="flex items-center gap-7 py-8 px-10">
+      <div className="flex-1">{children}</div>
+      {action}
+    </div>
+  );
+};
+
+export default Toolbar;
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export interface PageProps {}
+
+export default function Page({}: PageProps) {
+  return (
+    <main>
+      <h1 className="text-xl">Dashboard Page</h1>
+    </main>
+  );
+}
