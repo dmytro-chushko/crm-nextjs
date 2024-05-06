89ee45c3d6bd311c3d3003e65cbd91c84573c564
dmytro-chushko <dmytro.chushko@gmail.com>
2024-05-06 21:27:45 +0300

    Add react-query to the header-id page

--- src/app/(admin)/companies/@header/[id]/page.tsx
+++ src/app/(admin)/companies/@header/[id]/page.tsx
@@ -1,11 +1,23 @@
 import Header from '@/app/components/header';
+import { Company, getCompany } from '@/lib/api';
+import getQueryClient from '@/lib/utils/getQueryClient';
 
 interface IPageProps {
   params: { id: string };
 }
 
-const Page = ({ params }: IPageProps) => {
-  return <Header>{`Companies (${params.id})`}</Header>;
+const Page = async ({ params }: IPageProps) => {
+  const queryClient = getQueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ['companies', params.id],
+    queryFn: () => getCompany(params.id, { cache: 'no-store' }),
+    staleTime: 10 * 1000,
+  });
+
+  const company = queryClient.getQueryData(['companies', params.id]) as Company;
+
+  return <Header>{company?.title}</Header>;
 };
 
 export default Page;
