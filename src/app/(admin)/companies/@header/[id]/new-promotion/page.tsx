281cd60a18a4a92caa8288da4a6931034e703b7a
dmytro-chushko <dmytro.chushko@gmail.com>
2024-05-08 20:24:45 +0300

    Add promotion-form edit logo-uploader

---
+++ src/app/(admin)/companies/@header/[id]/new-promotion/page.tsx
@@ -0,0 +1,7 @@
+import Header from '@/app/components/header';
+
+const Page = () => {
+  return <Header>Add promotion company</Header>;
+};
+
+export default Page;
--- src/app/components/CompanyFormModal.tsx
+++ src/app/components/CompanyFormModal.tsx
@@ -4,17 +4,10 @@ import React from 'react';
 import CompanyForm, { CompanyFormProps } from '@/app/components/CompanyForm';
 import Modal, { ModalProps } from '@/app/components/Modal';
 
-export interface CompanyFormModalProps extends ModalProps {
-  onSubmit: CompanyFormProps['onSubmit'];
-}
-
-export default function CompanyFormModal({
-  onSubmit,
-  ...rest
-}: CompanyFormModalProps) {
+export default function CompanyFormModal({ onClose, ...rest }: ModalProps) {
   return (
-    <Modal {...rest}>
-      <CompanyForm onSubmit={onSubmit} />
+    <Modal {...rest} onClose={onClose}>
+      <CompanyForm onSubmit={() => onClose()} />
     </Modal>
   );
 }
